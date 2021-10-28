bf34c792947596de710142f8d6022a562f26169b
Castle <castlerock88@protonmail.com>
2021-10-28 13:44:06 -0400

    controllers config models

---
+++ controllers/todos.js
@@ -0,0 +1,50 @@
+const Todo = require('../models/modelTodo')
+
+module.exports = {
+	getTodos: async (req, res) => {
+		try{
+			const todoItems = await Todo.find()
+			const itemsLeft = await Todo.countDocuments({ completed: false })
+			res.render('todos.ejs', { todos: todoItems, left: itemsLeft })
+		}catch(err){
+			console.log(err)
+		}
+	},
+	createTodo: async (req, res) => {
+		try{
+			await Todo.create({ todo: req.body.todoItem, completed: false })
+			console.log('Todo has been added!')
+			res.redirect('/todos')
+		}catch(err){
+			console.log(err)
+		}
+	},
+	markComplete: async (req, res) => {
+		try{
+			await Todo.findOneAndUpdate({ _id: req.body.todoIdFromJSFile },{ completed: true })
+			console.log('marked complete')
+			res.json('marked complete')
+		}catch(err){
+			console.log(err)
+		}
+	},
+	markIncomplete: async (req, res) => {
+		try{
+			await Todo.findOneAndUpdate({ _id: req.body.todoIdFromJSFile }, {completed: false })
+			console.log('marked incomplete')
+			res.json('marked incomplete')
+		}catch(err){
+			console.log(err)
+		}
+	},
+	deleteTodo: async (req, res) => {
+		console.log(req.body.todoIdFromJSFile)
+		try{
+			await Todo.findOneAndDelete({ _id: req.body.todoIdFromJSFile })
+			console.log('Deleted todo')
+			res.json('Deleted it')
+		}catch(err){
+			console.log(err)
+		}
+	}
+}
